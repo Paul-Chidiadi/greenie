52e3765989586b7a91e4e96480e45b4e635ec8de
Paul Chidiadi <paulchidiadi@gmail.com>
2022-08-25 21:35:02 +0100

    created top section and it's media query adding images fo the background

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
   <?php include_once "head.php"?>
   <body>
-      <!-- HEADER SECTION -->
-      <header class="header" id="header">
+    <!-- HEADER SECTION -->
+    <header class="header" id="header">
         <nav class="navbar" id="navbar">
             <a href="index.php">
                 <img class="logo" src="img/logo.png" alt="">
@@ -20,19 +20,26 @@
                     <li>
                         <a href="#">TREES</a>
                     </li>
+                    <a  class="btn" href="#">SIGN UP</a>
                 </ul>
-            </div>
-            <div class="logbtn">
-                <a href="#">SIGN UP</a>
             </div>
             <div class="menubtn" id="menubtn">
                 <i class='bx bx-menu'></i>
                 <i class='bx bx-x'></i>
             </div>
-          </nav>
-      </header>
-  </body>
+        </nav>
+    </header>
+    <!-- BODY SECTION -->
+    <section class="top">
+        <div class="top-body">
+            <h1>GREENIE</h1>
+            <p>Be Inspired by Nature and Let's make our World Greener and Healthier</p>
+        </div>
+    </section>
 
-  // javascript files and libraries
-  <script type="text/javascript" src="js/index.js"></script>
+
+
+    <!-- javascript files and libraries -->
+    <script type="text/javascript" src="js/index.js"></script>
+  </body>
 </html>
