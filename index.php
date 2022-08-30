171a6ea03459a8da94ea0c982c163e0bc5d18f4d
Paul Chidiadi <paulchidiadi@gmail.com>
2022-08-30 21:11:49 +0100

    Footer section: added footer section with styling and media query

--- index.php
+++ index.php
@@ -203,10 +203,42 @@
             </div>
         </div>    
     </section>
-
-
-
-
+    <!-- FOOTER SECTION -->
+    <footer class="footer">
+        <div class="divide">
+            <img height="50px" width="120px" style="object-fit: cover" src="img/logo.png" alt="">
+            <img height="50px" width="120px" style="object-fit: cover" src="img/logo2.png" alt="">
+        </div>
+        <h4>COPYRIGHT © Greenie <script>document.write(new Date().getFullYear());</script>. ALL RIGHTS RESERVED. </h4>
+        <hr><br>
+        <div class="foot">
+            <div class="container">
+                <h3>ACTION</h3><br>
+                <div> 
+                    <a href="">-DONATE NOW</a>
+                    <a href="">-TREE STORE</a>
+                    <a href="">-SIGN UP</a>
+                </div>
+            </div>
+            <div class="container">
+                <h3>QUICK LINKS</h3><br>
+                <div>
+                    <a href="">-ABOUT</a>
+                    <a href="">-COMMUNITY</a>
+                    <a href="">-DONATIONS</a>
+                    <a href="">-TREES</a>
+                </div>
+            </div>
+            <div class="container">
+                <h3>CONNECT</h3><br>
+                <div>
+                    <a href="">-CONTACT US</a>
+                    <p>-<i class="bx bx-phone-call"></i> +234 9091910918</p>
+                    <p>-<i class="bx bxs-envelope"></i> greenienature@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </footer>
     <!-- javascript files and libraries -->
     <script type="text/javascript" src="js/index.js"></script>
   </body>
