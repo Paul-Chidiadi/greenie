8b93035676fa0dddc4d4472a8f9358b73e084bbe
Paul Chidiadi <paulchidiadi@gmail.com>
2022-09-12 20:43:21 +0100

    Store header: created header section with styling and media query, added tree.css too

--- store/tree.php
+++ store/tree.php
@@ -7,7 +7,7 @@
         <title>GREENIE STORE</title>
 
         <!--MAin CSS file-->
-        <link rel="stylesheet" href="css/index.css" />
+        <link rel="stylesheet" href="tree.css" />
         <!--BOXICONS CSS-->
         <link
         href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
@@ -15,6 +15,39 @@
         />
     </head>
     <body>
+        <!-- HEADER SECTTION -->
+        <header class="header" id="header">
+            <nav class="navbar" id="navbar">
+                <a href="index.php">
+                    <img class="logo" src="../img/logo.png" alt="">
+                </a>
+                <div class="menu" id="menu">
+                    <ul class="list">
+                        <li>
+                            <a href="#">LEARN</a>
+                        </li>
+                        <li class="sell">
+                            <a href="#">SELL</a>
+                            <div class="sale">
+                                <p>Upload a tree you would want to sell at our store by joining our
+                                community.</p>
+                                <br><a href="#">SIGN UP NOW</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="cart" id="cart">
+                                    <i class='bx bxs-cart-alt'></i>
+                                    <div class="top">
+                                        <p>5</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
         
     </body>
 </html>
